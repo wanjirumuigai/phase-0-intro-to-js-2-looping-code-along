d2e5b9ceb2143b9eb5c52e765f4598bc5ebc825d
Pauline Muigai <wanjirumuigai@gmail.com>
2022-11-20 12:45:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+//for loop
+let messages = [];
+
+function writeCards(names,wish) {
+    for (i=0; i<names.length; i++) {
+        messages.unshift(`Thank you, ${names[i]}, for the wonderful ${wish} gift`);
+    }
+    console.log(messages);
+
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+//while loop
+function countDown(number) {
+    
+    while (number>=0) {
+        console.log(number);
+        number --;
+
+    }
+    
+}
+countDown(10);
+
+  
